02e9b45365dacfc7fe52003709b5c4c8755a548d
Ishika Kataria <ishika-kataria@berkeley.edu>
2024-04-10 16:41:24 -0700

    updated resume

--- src/components/BoxResume.tsx
+++ src/components/BoxResume.tsx
@@ -2,7 +2,7 @@
 function BoxResume() {
     return (
         <div>
-           <iframe src="https://drive.google.com/file/d/1D0inv0lImobNsIX7Y-QzvL4JCa8Soc0b/preview" width="950" height="500" allow="autoplay"></iframe>
+           <iframe src="https://drive.google.com/file/d/16UAeYl1OEYtKw20VrEGf_DYLFWISauyd/preview" width="640" height="480" allow="autoplay"></iframe>
         </div>
     )
 }
