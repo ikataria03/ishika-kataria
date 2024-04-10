af82e78374eec424d69960bb836ed2efe78ec579
Ishika Kataria <ishika-kataria@berkeley.edu>
2024-04-10 10:31:44 -0700

    Fixed routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
 import './css/App.css';
-import {
-  Routes,
-  Route
-} from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './contents/Home';
 import Experience from './contents/Experience';
@@ -15,16 +12,18 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/experience" element={<Experience />} />
-        <Route path="/education" element={<Education />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/skills" element={<Skills />} />
-        <Route path="/resume" element={<Resume />} />
-      </Routes>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/experience" element={<Experience />} />
+          <Route path="/education" element={<Education />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/resume" element={<Resume />} />
+        </Routes>
+      </Router>
     </div>
-
+    
   );
 }
 
